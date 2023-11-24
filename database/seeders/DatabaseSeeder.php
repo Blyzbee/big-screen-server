b66081c08fb813587af488d7190397c0bc81a16d
NateDev <jonathanmbaya13@gmail.com>
2023-11-24 11:41:07 +0100

    Factory et Seeders User table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,14 +2,12 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UsersTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
@@ -18,5 +16,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            UsersTableSeeder::class
+        ]);
     }
 }
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // Création d'un utilisateur administrateur
+        DB::table('users')->insert([
+            'email' => 'admin@example.com',
+            'password' => Hash::make('adminpassword'),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        // Création de plusieurs utilisateurs avec la factory
+        // \App\Models\User::factory(10)->create();
+    }
+}
